0b27614caa1be6d1b8511f1a4840539a4486c50c
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-05 12:25:45 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
+// 1. NaN : Not a Number;
+//1.1.
+console.log('1.1. '+isNaN("hello"));
+console.log(isNaN(3));
+console.log(typeof NaN);
+//2.
+console.log('2. ' + (0.1*0.2).toFixed(2));
+
+//3.
+console.log('3. ' + Infinity/0);
+console.log(Infinity/Infinity);
+console.log(1/0);
 
